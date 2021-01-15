95c797bda481aeccd1b97371a89a6c53c1bbb478
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2021-01-16 00:12:09 +0530

    changes done

--- php/causelist/EntireCauseList.php
+++ php/causelist/EntireCauseList.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=1024">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css?v=<?php echo time(); ?>"/>
     <link href="../../css/seaching.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
