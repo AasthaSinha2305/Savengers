0619ba3f2a64d0337a7eabd378546043866e44d6
Anamika810 <55596686+Anamika810@users.noreply.github.com>
2021-01-16 14:35:13 +0530

    Update judgeWise.php

--- html/php/causelist/judgeWise.php
+++ html/php/causelist/judgeWise.php
@@ -7,9 +7,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="../../../css/seaching.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    <style>
+.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 50px 50px;
+  text-align: center;
+  }
+  body{
+    margin: 0px;
+    padding: 0px;
+    text-align:center;
+    width: 100%;
+    background-color: #e6e6fae8;
+    }
+    button{
+    background-color:#4CAF50;
+    width: 10%;
+    padding: 9px 5px;
+    margin:10px 0px 0px 35px;
+    cursor:pointer;
+    border:none;
+    color:#ffffff;
+    font-size: 15px;
+    font-weight: bold;
+    }
+     
+    button:hover{
+    opacity:0.8;
+    }
+     
+    #un,#ps{
+    font-family:'Lato', sans-serif;
+    color: gray;
+    }
+ 
+ 
+    #container{
+        position: absolute;
+        top:0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        width:600;
+        height: 300px;
+        text-align: center;
+    }
+</style>
 </head>
 
 <body>
+    <div class="header"><img src="https://www.ritiriwaz.com/wp-content/uploads/2017/01/Indian-Emblem.jpg" alt="National Embelem" height="100px" width="100px" align="middle">
+</div> 
     <center>
     <h3>Judge Name Wise</h3>
         <form action="" method="POST">
@@ -33,7 +83,7 @@
             <option value="anamika">Mr. Justice Anamika</option>
         </select>
         <br><br>
-            <input type="submit" name="search" value="Search">
+            <button type="submit" name="search" value="Search">SEARCH</button>
         </form>
     </center>
 </body>
