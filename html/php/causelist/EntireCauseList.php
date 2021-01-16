ed2c796eb5954d489556e95423188c3099653637
Anamika810 <55596686+Anamika810@users.noreply.github.com>
2021-01-16 14:27:57 +0530

    Update EntireCauseList.php

--- html/php/causelist/EntireCauseList.php
+++ html/php/causelist/EntireCauseList.php
@@ -8,6 +8,65 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css?v=<?php echo time(); ?>"/>
     <link href="../../../css/seaching.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    <style>
+        .header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 50px 50px;
+  text-align: center;
+  }
+  body{
+    margin: 0px;
+    padding: 0px;
+    text-align:center;
+    width: 100%;
+    background-color: #e6e6fae8;
+    }
+     
+    input[type=text]{
+    width:20%;
+    padding:7px 10px;
+    margin: 10px 0;
+    display:inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    }
+     
+    button{
+    background-color:#4CAF50;
+    width: 10%;
+    padding: 9px 5px;
+    margin:10px 0px 0px 35px;
+    cursor:pointer;
+    border:none;
+    color:#ffffff;
+    font-size: 15px;
+    font-weight: bold;
+    }
+     
+    button:hover{
+    opacity:0.8;
+    }
+     
+    #un,#ps{
+    font-family:'Lato', sans-serif;
+    color: gray;
+    }
+ 
+ 
+    #container{
+        position: absolute;
+        top:0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        width:600;
+        height: 300px;
+        text-align: center;
+    }
+    </style>
+        
     
 </head>
 
@@ -24,7 +83,8 @@
             <option value="2021-01-01">2021-01-01</option>
         </select>
         <br><br>
-            <input type="submit" name="search" value="Search">
+            <button type="submit" name="search" value="Search">SEARCH</button>
+            <a href="causelist.html">Back</a>
         </form>
     </center>
 </body>
