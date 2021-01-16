10cc5d7b5b84946c97810342ffbb90c095b228f4
Anamika810 <55596686+Anamika810@users.noreply.github.com>
2021-01-16 14:36:44 +0530

    Update aorWise.php

--- html/php/causelist/aorWise.php
+++ html/php/causelist/aorWise.php
@@ -69,6 +69,8 @@
 </head>
 
 <body>
+    <div class="header"><img src="https://www.ritiriwaz.com/wp-content/uploads/2017/01/Indian-Emblem.jpg" alt="National Embelem" height="100px" width="100px" align="middle">
+</div> 
     <center>
     <h3>AOR Wise</h3>
         <form action="" method="POST">
