9587b0e046d71a5ce007ff470f3a0f7d67e6a414
k-akash-i <akashn.ug19.cs@nitp.ac.in>
2021-01-16 12:26:33 +0530

    .

--- html/php/causelist/aorWise.php
+++ html/php/causelist/aorWise.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link href="../../../css/seaching.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
 </head>
 
