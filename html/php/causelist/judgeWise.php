dfc65e80eef63f289def3f60c1f5eef0738d8d99
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2021-01-18 05:28:21 +0530

    --

--- html/php/causelist/judgeWise.php
+++ html/php/causelist/judgeWise.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../../css/seaching.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <title>Document</title>
     <style>
 .header {
